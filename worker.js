c4059cdedf71ecd51b1b0aa2379e198f9a34460e
Anmol Shukla <anmol.28422@gmail.com>
2024-02-13 18:44:11 +0530

    worker mail confirmation

--- worker.js
+++ worker.js
@@ -1,8 +1,12 @@
 const { Worker } = require("bullmq");
 
+const sendEmail = () =>
+  new Promise((res, rej) => setTimeout(() => res(), 5 * 1000));
+
 const worker = new Worker("email-queue", async (job) => {
   console.log(`Message Rec with id: ${job.id}`);
   console.log("Processing Message...");
   console.log(`Sending email to ${job.data.email}`);
-  return new Promise((res, rej) => setTimeout(() => res(), 5 * 1000));
-});
+  await sendEmail();
+  console.log("Email Sent");
+}).run();
